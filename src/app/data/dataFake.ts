13146d6d64bced061707712cf14be50ea57b300d
datdudu <c.eduardocarvalhoc@gmail.com>
2023-09-06 23:07:40 -0300

    Adding routing for the pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const datFake = [
+  {
+    "id": 0,
+    "photo": "https://guiadeti.com.br/wp-content/uploads/2022/01/guia-cursos-dio.png",
+    "title": "Projeto feito no bootcamp da DIO",
+    "description": "Projeto de um blog feito no bootcamp de Fullstack da Digital Innovation One"
+  },
+  {
+    "id": 1,
+    "photo": "https://miro.medium.com/v2/resize:fit:480/1*9A6E9kaZZ54idy0HLSlh-A.png",
+    "title": "Design aplicado utilizando Angular",
+    "description": "O framework Angular foi utilizado para fazer este blog"
+  },
+  {
+    "id": 2,
+    "photo": "https://raw.githubusercontent.com/khaosdoctor/blog-assets/master/images/2022/06/10b88c68-typescript-logo.png",
+    "title": "Projeto Utiliza Typescript",
+    "description": "Projeto utiliza typescript para dar algumas funcionalidades e dinamismo para o conteúdo do blog"
+  },
+  {
+    "id": 3,
+    "photo": "https://www.brasilcode.com.br/wp-content/uploads/2020/05/HTML-e-CSS-Guia-basico-1024x427.png",
+    "title": "Projeto Utiliza HTML e CSS",
+    "description": "Projeto utiliza HTML e CSS para a criação do esquelo e estilo dos componentes"
+  },
+]
